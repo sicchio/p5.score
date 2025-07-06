801cfb745e6e39588181f1384c376dbecbcb39b5
Kate <kate@sicchio.com>
2025-07-06 16:55:42 -0400

    Create doodles.js
    little sample functions

---
+++ doodles.js
@@ -0,0 +1,50 @@
+let stageDirection = {
+  x: 1,
+  y: -1,
+  xspeed: 1,
+  yspeed: 1,
+  sustain: 0.15
+};
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  
+  // dancer(50, 50);
+  // move();
+  
+dancer2(100,150);
+ sustain();
+}
+
+function dancer(x, y) {
+  stroke(0, 255, 250);
+  strokeWeight(4);
+  noFill();
+  ellipse(stageDirection.x, y, 50, 50);
+}
+
+function dancer2(x, y) {
+  stroke(255, 0, 250);
+  strokeWeight(4);
+  noFill();
+  ellipse(stageDirection.x, y, 50, 50);
+}
+
+function move() {
+  line(10,50,150,50);
+  if (stageDirection.x < 150){
+  stageDirection.x = stageDirection.x + stageDirection.xspeed;
+  }
+}
+
+function sustain() {  
+  for (x = 10;  x < 150; x = x+ 10){ line(x,150,x,150);
+                                   }  
+  if (stageDirection.x < 250){
+  stageDirection.x = stageDirection.x + stageDirection.sustain;
+  }
+}
